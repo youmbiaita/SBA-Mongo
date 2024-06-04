58d632d2510eeac44827f42ec1e167f0db9c9ac4
Tchakounteaita <164936530+youmbiaita@users.noreply.github.com>
2024-06-03 23:04:14 -0400

    change const to async functions

--- controllers/users.js
+++ controllers/users.js
@@ -1,15 +1,77 @@
 const User = require('../models/user');
 
 module.exports = {
+  validateUser,
+  getAllUsers,
   createUser,
+  getUserById,
+  updateUserById,
+  deleteUserById,
+};
+
+const validateUser = (req, res, next) => {
+  const { name, email } = req.body;
+  let message = "";
+  if (!name) message += 'name, ';
+  if (!email) message += 'email.';
+
+  if (message) {
+    return res.status(400).send("Missing field(s) required: " + message);
+  }
+  next();
+};
+
+async function getAllUsers(req, res) {
+  try {
+    const users = await User.find();
+    res.json(users);
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
 };
 
 async function createUser(req, res) {
-  try {
-    const user = await User.create(req.body);
+  const { name, email } = req.body;
+  const user = new User({ name, email });
 
-    res.status(200).json(user);
+  try {
+    await user.save();
+    res.status(201).json(user);
   } catch (err) {
-    res.status(400).json('No Beuno:(');
+    res.status(500).send(err.message);
   }
-}
+};
+
+async function getUserById(req, res) {
+  try {
+    const user = await User.findById(req.params.id);
+    if (!user) return res.status(404).send('User not found');
+    res.json(user);
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+};
+
+async function updateUserById(req, res) {
+  const { name, email } = req.body;
+
+  try {
+    const user = await User.findByIdAndUpdate(req.params.id, { name, email }, { new: true });
+    if (!user) return res.status(404).send('User not found');
+    res.json(user);
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+};
+
+async function deleteUserById(req, res) {
+  try {
+    const user = await User.findByIdAndDelete(req.params.id);
+    if (!user) return res.status(404).send('User not found');
+    res.json(user);
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+};
+
+
